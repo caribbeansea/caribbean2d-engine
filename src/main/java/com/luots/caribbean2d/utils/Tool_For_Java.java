94fdcee502c1a40ff9b06e842bb053a7be8f0a06
2B键盘 <2968640488@qq.com>
2021-01-17 21:45:42 +0800

    feat: 新增精灵对象

---
+++ src/main/java/com/luots/caribbean2d/utils/Tool_For_Java.java
@@ -0,0 +1,42 @@
+package com.luots.caribbean2d.utils;
+
+/* ************************************************************************
+ *
+ * Copyright (C) 2020 caribbeansea All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ************************************************************************/
+
+/*
+ * Creates on 2021/1/7.
+ */
+
+/**
+ * @author tiansheng
+ */
+public class Tool_For_Java
+{
+
+    public static void __Double_For__(int a, int b, Double_For_Achieve achieve)
+    {
+        for (int i = 0; i < a; i++)
+        {
+            for (int j = 0; j < b; j++)
+            {
+                achieve.achieve(i, j);
+            }
+        }
+    }
+
+}
